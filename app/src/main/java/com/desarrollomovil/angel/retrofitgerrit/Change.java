c5548541870e54310ba75ccaf9dce29a7a9666b1
angel <miltonacomer@openmailbox.org>
2017-01-27 16:48:51 +0100

    Initial commit

---
+++ app/src/main/java/com/desarrollomovil/angel/retrofitgerrit/Change.java
@@ -0,0 +1,18 @@
+package com.desarrollomovil.angel.retrofitgerrit;
+
+/**
+ * Created by angel on 27/01/2017.
+ */
+
+public class Change {
+
+    String subject;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
